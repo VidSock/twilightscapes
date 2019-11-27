b52dc5046654b3cc1f810f5ea4b8f39902eceb1e
twilightscapes <ihatespam@gmail.com>
2019-11-27 02:46:17 -0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -30,13 +30,13 @@ const Footer = class extends React.Component {
 
 <ul className="socialmenu" style={{textAlign:'center', justifyContent:'center', margin:'3rem 0'}}>
   <li><h3 className="socialtext">I&apos;m<br />Social:</h3></li>
-<li><a className="social"  href="https://facebook.com" alt="Facebook" title="Facebook"><FaFacebookSquare /><span>Facebook</span></a></li>
-<li style={{display: 'none',}}><a className="social" href="https://linkedin.com" alt="LinkIn" title="LinkedIn"><FaLinkedin /><span>LinkedIn</span></a></li>
-<li><a className="social" href="https://twitter.com" alt="Twitter" title="Twitter"><FaTwitterSquare /><span>Twitter</span></a></li>
-<li><a className="social" href="https://instagram.com" alt="Instagram" title="Instagram"><FaInstagram /><span>Instagram</span></a></li>
-<li><a className="social" href="https://pinterest.com" alt="Pinterest" title="Pinterest"><FaPinterestSquare /><span>Pinterest</span></a></li>
+<li><a className="social"  href="https://facebook.com/twilightscapes" alt="Facebook" title="Facebook"><FaFacebookSquare /><span>Facebook</span></a></li>
+<li style={{display: 'none',}}><a className="social" href="https://www.linkedin.com/in/toddlambert" alt="LinkIn" title="LinkedIn"><FaLinkedin /><span>LinkedIn</span></a></li>
+<li><a className="social" href="https://twitter.com/toddlambert" alt="Twitter" title="Twitter"><FaTwitterSquare /><span>Twitter</span></a></li>
+<li><a className="social" href="https://www.instagram.com/twilightscapes" alt="Instagram" title="Instagram"><FaInstagram /><span>Instagram</span></a></li>
+<li><a className="social" href="https://www.pinterest.com/twilightscapes/" alt="Pinterest" title="Pinterest"><FaPinterestSquare /><span>Pinterest</span></a></li>
 
-<li><a className="social" href="https://youtube.com" alt="YouTube" title="YouTube"><FaYoutubeSquare /><span>YouTube</span></a></li>
+<li><a className="social" href="https://www.youtube.com/channel/UCccK2S9xrcLnCR3dTuUwsxQ" alt="YouTube" title="YouTube"><FaYoutubeSquare /><span>YouTube</span></a></li>
 
 
 
